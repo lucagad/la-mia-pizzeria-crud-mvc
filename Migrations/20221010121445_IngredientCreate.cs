f582845aa382e7fbd8430cb4d46439243609eb67
lucagaddini <lucagaddini@icloud.com>
2022-10-10 15:25:38 +0200

    Modificata Vista Create

---
+++ Migrations/20221010121445_IngredientCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace la_mia_pizzeria_crud_mvc.Migrations
+{
+    public partial class IngredientCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ingredient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ingredient", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientPizza",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<int>(type: "int", nullable: false),
+                    PizzasPizzaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientPizza", x => new { x.IngredientsId, x.PizzasPizzaId });
+                    table.ForeignKey(
+                        name: "FK_IngredientPizza_ingredient_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "ingredient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientPizza_pizza_PizzasPizzaId",
+                        column: x => x.PizzasPizzaId,
+                        principalTable: "pizza",
+                        principalColumn: "PizzaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientPizza_PizzasPizzaId",
+                table: "IngredientPizza",
+                column: "PizzasPizzaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientPizza");
+
+            migrationBuilder.DropTable(
+                name: "ingredient");
+        }
+    }
+}
